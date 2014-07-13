458c337ecaf27579c42347b8e238aaaeda27d17a
Mike <mbarton.mail@gmail.com>
2014-07-13 16:45:22 -0700

    Add comments to friends page and controller

--- www/pages/friends/friends.js
+++ www/pages/friends/friends.js
@@ -1,6 +1,9 @@
 angular.module('starter.friends', [])
+
+//The controller for the friends page.
 .controller('FriendsCtrl', function($scope, $state) {
   $scope.toGame = function() {
+  	//Link to game page
     $state.go('game');
   };
 });
