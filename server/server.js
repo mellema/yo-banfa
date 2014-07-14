f35215fe0619c7a9249c256de15f9161fa9f1e67
Hack Reactor Students <accounts@hackreactor.com>
2014-07-14 12:01:22 -0700

    implement basic server

--- server/server.js
+++ server/server.js
@@ -1,32 +1,38 @@
-// The main application script, ties everything together.
+  // The main application script, ties everything together.
 
 var express = require('express');
 var mongoose = require('mongoose');
-var app = module.exports = express.createServer();
-var api = require('./controllers/api.js');
+//Security risk with body parser?
+var bodyParser = require('body-parser');
+var app = module.exports = express();
+var api = require('./api/controllers.js');
 
 // connect to Mongo when the app initializes
-mongoose.connect('mongodb://localhost/norum');
+mongoose.connect('mongodb://localhost/api');
 
 //Configure our express server instance
-app.configure(function(){
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-});
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+//MethodOvverride?
+var router = express.Router();
 
 //Set up our port
 var port = process.env.PORT || 9000;
 
+
 // set up the RESTful API, handler methods are defined in api.js
-app.post('/thread', api.post);
-app.get('/thread/:title.:format?', api.show);
-app.get('/thread', api.list);
+router.post('/thread', api.post);
+router.get('/thread/:title.:format?', api.show);
+router.get('/thread', api.list);
+
+app.use('/', router);
 
 
 //Listen on port and log status
 app.listen(port);
-console.log("Express server listening on port %d", app.address().port);
+console.log("Express server listening on port ", port);
 
 
 
