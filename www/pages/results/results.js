66d1003d2e12d3edda22e20baa5452e6cdca12ba
wcjohnson11 <williamcouperjohnson@gmail.com>
2014-07-15 08:02:33 -0700

    resolve type in results

--- www/pages/results/results.js
+++ www/pages/results/results.js
@@ -1,6 +1,6 @@
 angular.module('starter.results', [])
 .controller('ResultsCtrl', function($scope, $state) {
-  $scope.enter = function() {
+  $scope.return = function() {
     $state.go('friends');
   };
 });
