b38e7e5cab8504762b801ce0ac29d14af64f8c57
wcjohnson11 <williamcouperjohnson@gmail.com>
2014-07-15 11:48:33 -0700

    adding card api structure

---
+++ server/config/seed.js
@@ -0,0 +1,40 @@
+
+//Require the 
+var Card = require('../api/card/card.model');
+
+Card.find({}).remove(function() {
+  Card.create({
+    simplehanzi: '中国',
+    tradHanzi: '中國',
+    pinyin: 'Zhōng guó',
+    translation: 'China',
+    info: 'Countries'
+  },{
+    simplehanzi: '美国',
+    tradHanzi: '美國',
+    pinyin: 'Měi guó',
+    translation: 'America',
+    info: 'Countries'
+  },{
+    simplehanzi: '马里',
+    tradHanzi: '馬里',
+    pinyin: 'Mǎ lǐ',
+    translation: 'Mali',
+    info: 'Countries'
+  },{
+    simplehanzi: '日本',
+    tradHanzi: '日本',
+    pinyin: 'Rì běn',
+    translation: 'Japan',
+    info: 'Countries'
+  }, {
+    simplehanzi: '德国',
+    tradHanzi: '德國',
+    pinyin: 'Dé guó',
+    translation: 'Germany',
+    info: 'Countries'
+  }, function() {
+      console.log('finished populating cards');
+    }
+  );
+});
