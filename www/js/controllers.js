e59a54032d081b3ed4c46700d12ebc390b865bd9
Walker Mellema <walkermellema@gmail.com>
2014-07-15 22:21:43 -0700

    add mock side menu

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,4 +5,15 @@ angular.module('starter.controllers', ['starter.frontPage', 'starter.friends', '
 /*.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
   $scope.friend = Friends.get($stateParams.friendId);
 })
+<<<<<<< HEAD
 */
+
+.controller('MenuController', function ($scope, $location, MenuService) {
+  // "MenuService" is a service returning mock data (services.js)
+  $scope.list = MenuService.all();
+
+  // $scope.goTo = function(page) {
+  //   console.log('Going to ' + page);
+  //   $location.url('/' + page);
+  // };
+});
