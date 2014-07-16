437f7e595c7a53fd59236148258d6138e7fe0431
Walker Mellema <walkermellema@gmail.com>
2014-07-16 13:56:10 -0700

    start oAuth stuff

--- server/server.js
+++ server/server.js
@@ -2,8 +2,23 @@
 
 var express = require('express');
 var mongoose = require('mongoose');
+var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
 
 var app = express();
+// var app.use(passport.initialize());
+passport.use(new FacebookStrategy({
+    clientID: 648798351882921,
+    clientSecret: fd19734e0be95df8f41499bbe345406e,
+    callbackURL: "http://www.example.com/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate(..., function(err, user) {
+      if (err) { return done(err); }
+      done(null, user);
+    });
+  }
+));
 //Security risk with body parser?
 
 // connect to Mongo when the app initializes
