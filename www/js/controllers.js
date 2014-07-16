59bf3f284529fd22eb180cdb1907895add52ae02
Mike <mbarton.mail@gmail.com>
2014-07-15 21:08:31 -0700

    Clean up old files.  Add auth folder

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,13 +1,8 @@
 
 angular.module('starter.controllers', ['starter.frontPage', 'starter.friends', 'starter.game', 'starter.results', 'starter.hanziOptions', 'starter.deckOptions'])
 
-.controller('DashCtrl', function($scope) {
-})
 
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+/*.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
   $scope.friend = Friends.get($stateParams.friendId);
 })
-
-.controller('AccountCtrl', function($scope) {
-});
-
+*/
