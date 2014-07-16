3b9afcee23c5a0a32a0f54fa5b8ba6b8c2d685e0
Walker Mellema <walkermellema@gmail.com>
2014-07-16 08:02:54 -0700

    try to resolve rebase problem

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,11 +1,9 @@
-
 angular.module('starter.controllers', ['starter.frontPage', 'starter.friends', 'starter.game', 'starter.results', 'starter.hanziOptions', 'starter.deckOptions'])
 
 
 /*.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
   $scope.friend = Friends.get($stateParams.friendId);
 })
-<<<<<<< HEAD
 */
 
 .controller('MenuController', function ($scope, $location, MenuService) {
