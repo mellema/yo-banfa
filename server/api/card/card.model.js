a3c5274d28981c9514d50db2646af90f3d8a0871
wcjohnson11 <williamcouperjohnson@gmail.com>
2014-07-17 16:51:56 -0700

    card model updated to populate db

--- server/api/card/card.model.js
+++ server/api/card/card.model.js
@@ -1,15 +1,66 @@
 
-var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
+var mongoose = require('mongoose');
+var fs = require('fs');
+// Create a connection instance at db localhost and table mydb
+mongoose.createConnection('localhost', 'mydb');
 
-var CardSchema = new Schema({
+// Load the csv file into lineList and split by line
+var lineList = fs.readFileSync('hskLevel1.csv').toString().split('\n');
+// Remove the headers
+lineList.shift();
+
+// Create the list of schemae keys for docRecurse **ORDER MATTERS**
+var schemaKeyList = ['card_ID','groupName','simpleHanzi','tradHanzi','pinyin','english'];
+
+
+var CardSchema = new mongoose.Schema({
+  card_ID: Number,
+  groupName: String,
   simpleHanzi: String,
   tradHanzi: String,
-  card_ID: Number,
   pinyin: String,
-  english: String,
-  groupName: String,
-  active: Boolean
+  english: String
 });
 
+// Export card model for use in app
 module.exports = mongoose.model('Card', CardSchema);
+
+// For use in populating db
+var CardDoc = mongoose.model('Card', CardSchema);
+
+// Aggregate a list of all items from the file
+function queryAllEntries () {
+    CardDoc.aggregate(
+        {$group: {_id: '$card_ID',
+            groupName: '$groupName',
+            simpleHanzi: '$simpleHanzi',
+            tradHanzi: '$tradHanzi',
+            pinyin: '$pinyin',
+            english: '$english'
+        }}, function(err, qDocList) {
+        console.log(qDocList);
+        // process.exit(0);
+    });
+}
+
+// Recursively go through list adding documents.
+
+function createDocRecurse (err) {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
+    if (lineList.length) {
+        var line = lineList.shift();
+        var doc = new CardDoc();
+        line.split(',').forEach(function (entry, i) {
+            doc[schemaKeyList[i]] = entry;
+        });
+        doc.save(createDocRecurse);
+    } else {
+        // After the last entry query to show the result.
+        queryAllEntries();
+    }
+}
+
+createDocRecurse(null);
