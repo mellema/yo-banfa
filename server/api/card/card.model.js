997e33584559b4d9353c4a96222b12ef461a017d
Walker Mellema <walkermellema@gmail.com>
2014-07-18 11:57:41 -0700

    change port back to 9000, comment out card.model.js

--- server/api/card/card.model.js
+++ server/api/card/card.model.js
@@ -4,64 +4,64 @@ var fs = require('fs');
 // Create a connection instance at db localhost and table mydb
 mongoose.createConnection('localhost', 'mydb');
 
-// Load the csv file into lineList and split by line
-var lineList = fs.readFileSync('hskLevel1.csv').toString().split('\n');
-// Remove the headers
-lineList.shift();
+// // Load the csv file into lineList and split by line
+// var lineList = fs.readFileSync('hskLevel1.csv').toString().split('\n');
+// // Remove the headers
+// lineList.shift();
 
-// Create the list of schemae keys for docRecurse **ORDER MATTERS**
-var schemaKeyList = ['card_ID','groupName','simpleHanzi','tradHanzi','pinyin','english'];
+// // Create the list of schemae keys for docRecurse **ORDER MATTERS**
+// var schemaKeyList = ['card_ID','groupName','simpleHanzi','tradHanzi','pinyin','english'];
 
 
-var CardSchema = new mongoose.Schema({
-  card_ID: Number,
-  groupName: String,
-  simpleHanzi: String,
-  tradHanzi: String,
-  pinyin: String,
-  english: String
-});
+// var CardSchema = new mongoose.Schema({
+//   card_ID: Number,
+//   groupName: String,
+//   simpleHanzi: String,
+//   tradHanzi: String,
+//   pinyin: String,
+//   english: String
+// });
 
-// Export card model for use in app
-module.exports = mongoose.model('Card', CardSchema);
+// // Export card model for use in app
+// module.exports = mongoose.model('Card', CardSchema);
 
-// For use in populating db
-var CardDoc = mongoose.model('Card', CardSchema);
+// // For use in populating db
+// var CardDoc = mongoose.model('Card', CardSchema);
 
-// Aggregate a list of all items from the file
-function queryAllEntries () {
-    CardDoc.aggregate(
-        {$group: {_id: '$card_ID',
-            groupName: '$groupName',
-            simpleHanzi: '$simpleHanzi',
-            tradHanzi: '$tradHanzi',
-            pinyin: '$pinyin',
-            english: '$english'
-        }}, function(err, qDocList) {
-        console.log(qDocList);
-        // process.exit(0);
-    });
-}
+// // Aggregate a list of all items from the file
+// function queryAllEntries () {
+//     CardDoc.aggregate(
+//         {$group: {_id: '$card_ID',
+//             groupName: '$groupName',
+//             simpleHanzi: '$simpleHanzi',
+//             tradHanzi: '$tradHanzi',
+//             pinyin: '$pinyin',
+//             english: '$english'
+//         }}, function(err, qDocList) {
+//         console.log(qDocList);
+//         // process.exit(0);
+//     });
+// }
 
-// Recursively go through list adding documents.
+// // Recursively go through list adding documents.
 
-function createDocRecurse (err) {
-    if (err) {
-        console.log(err);
-        process.exit(1);
-    }
-    if (lineList.length) {
-        var line = lineList.shift();
-        var doc = new CardDoc();
-        line.split(',').forEach(function (entry, i) {
-            doc[schemaKeyList[i]] = entry;
-        });
-        doc.save(createDocRecurse);
-    } else {
-        // After the last entry query to show the result.
-        queryAllEntries();
-    }
-}
+// function createDocRecurse (err) {
+//     if (err) {
+//         console.log(err);
+//         process.exit(1);
+//     }
+//     if (lineList.length) {
+//         var line = lineList.shift();
+//         var doc = new CardDoc();
+//         line.split(',').forEach(function (entry, i) {
+//             doc[schemaKeyList[i]] = entry;
+//         });
+//         doc.save(createDocRecurse);
+//     } else {
+//         // After the last entry query to show the result.
+//         queryAllEntries();
+//     }
+// }
 
 createDocRecurse(null);
 */
