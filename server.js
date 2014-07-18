d6e8dfd08b424f8a08b54d2a591d3ed6a51ac928
William Johnson <williamcouperjohnson@gmail.com>
2014-07-18 11:22:12 -0700

    Revert server to previous settings

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = require('./server/server.js');
 
 //Set up our port
 // var port = process.env.PORT || 9000;
-app.listen(process.env.PORT || 5000, function() {
+app.listen(process.env.PORT || 9000, function() {
 	console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 //Listen on port and log status
