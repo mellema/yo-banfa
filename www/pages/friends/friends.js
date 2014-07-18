5b977a103d62b681a21b41371e12e0fafd7916be
Mike <mbarton.mail@gmail.com>
2014-07-18 13:18:39 -0700

    Start game now generates a game in the database and stores game's _id on localstorage

--- www/pages/friends/friends.js
+++ www/pages/friends/friends.js
@@ -1,8 +1,9 @@
 angular.module('starter.friends', [])
 
 //The controller for the friends page.
-.controller('FriendsCtrl', function($scope, $state, $window, Friends) {
+.controller('FriendsCtrl', function($scope, $state, $window, Friends, Game) {
   $scope.getFriends = Friends.getFriends;
+  $scope.makeGame = Game.makeGame
   $scope.data = {};
   //uncomment this and use instead of johnDoe
   //$scope.data.user =  $window.localStorage.getItem('yobanfaUsername'); 
@@ -12,7 +13,13 @@ angular.module('starter.friends', [])
   });
 
   $scope.toGame = function() {
-  	//Link to game page
-    $state.go('hanziOptions');
+    //Make game
+    $scope.makeGame({creator: 'me', challenged: 'you'}).then(function(resp){
+      console.log(resp.data)
+      //Save game id
+      $window.localStorage.setItem('currentGame', resp.data._id);
+    	//Link to game page
+      $state.go('hanziOptions');
+    })
   };
 });
