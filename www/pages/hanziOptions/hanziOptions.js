e1482ed43401be35f8ff03822a8353b3314602d0
wcjohnson11 <williamcouperjohnson@gmail.com>
2014-07-19 13:10:40 -0700

    storing hanziOptions in localstorage

--- www/pages/hanziOptions/hanziOptions.js
+++ www/pages/hanziOptions/hanziOptions.js
@@ -10,15 +10,7 @@ angular.module('starter.hanziOptions', [])
 	return LS.getData();
   };
   $scope.update = function(val) {
-<<<<<<< HEAD
 	console.log('value', val);
 	return LS.setData(val);
-=======
-	return LS.setData(val);
-  };
-  $scope.goDecks = function(val) {
-	LS.setData(val);
-    $state.go('../deckOptions/deckOptions.html');
->>>>>>> refactor Hanzi options
   };
 });
