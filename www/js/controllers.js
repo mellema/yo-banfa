483b86cbd951c0d57f157175d02f937543925bb9
wcjohnson11 <williamcouperjohnson@gmail.com>
2014-07-19 12:23:59 -0700

    refactor Hanzi options

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,23 @@
 angular.module('starter.controllers', ['starter.frontPage', 'starter.friends', 'starter.game', 'starter.results', 'starter.hanziOptions', 'starter.deckOptions'])
 
 
-/*.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
+.factory('LS', function($window, $rootScope) {
+  angular.element($window).on('storage', function(event) {
+	if (event.key === 'my-storage') {
+		$rootScope.apply();
+	}
+  });
+  return {
+	setData: function(val) {
+      $window.localStorage && $window.localStorage.setItem('my-storage', val);
+      return this;
+	},
+	getData: function() {
+      return $window.localStorage && $window.localStorage.getItem('my-storage');
+	}
+  };
 })
-*/
+
 
 .controller('MenuController', function ($scope, $location, MenuService) {
   // "MenuService" is a service returning mock data (services.js)
--- www/pages/hanziOptions/hanziOptions.js
+++ www/pages/hanziOptions/hanziOptions.js
@@ -1,6 +1,19 @@
 angular.module('starter.hanziOptions', [])
-.controller('HanziOptionsCtrl', function($scope, $state) {
-  $scope.goDecks = function() {
+.controller('HanziOptionsCtrl', function($scope, $state, LS) {
+  $scope.value = LS.getData();
+  $scope.options = [
+    {title: 'Traditional', hanzi: '繁體字'},
+    {title: 'Simplified', hanzi: '简体字'},
+    {title: 'Pinyin', hanzi: 'pīnyīn'}
+  ];
+  $scope.latestData = function() {
+	return LS.getData();
+  };
+  $scope.update = function(val) {
+	return LS.setData(val);
+  };
+  $scope.goDecks = function(val) {
+	LS.setData(val);
     $state.go('../deckOptions/deckOptions.html');
   };
 });
