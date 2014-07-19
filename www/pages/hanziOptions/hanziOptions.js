eff7de48f8c673042556698471e7d5278ac01e17
wcjohnson11 <williamcouperjohnson@gmail.com>
2014-07-19 13:27:37 -0700

    refactor hanziOptions

--- www/pages/hanziOptions/hanziOptions.js
+++ www/pages/hanziOptions/hanziOptions.js
@@ -1,6 +1,16 @@
 angular.module('starter.hanziOptions', [])
-.controller('HanziOptionsCtrl', function($scope, $state) {
-  $scope.goDecks = function() {
-    $state.go('../deckOptions/deckOptions.html');
+.controller('HanziOptionsCtrl', function($scope, $state, LS) {
+  $scope.value = LS.getData();
+  $scope.options = [
+    {title: 'Traditional', hanzi: '繁體字'},
+    {title: 'Simplified', hanzi: '简体字'},
+    {title: 'Pinyin', hanzi: 'pīnyīn'}
+  ];
+  $scope.latestData = function() {
+	return LS.getData();
+  };
+  $scope.update = function(val) {
+	console.log('value', val);
+	return LS.setData(val);
   };
 });
