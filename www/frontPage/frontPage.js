7eaad7bd47c28227f0e69e9a8cd4de7e402e281b
Walker Mellema <walkermellema@gmail.com>
2014-07-19 11:58:24 -0700

    add localStorage for authentication

--- www/frontPage/frontPage.js
+++ www/frontPage/frontPage.js
@@ -3,16 +3,26 @@ angular.module('starter.frontPage', [])
   //temporary fix for oauth not working
   $scope.user = {};
   $scope.signin = function (isValid) {
-    if( !isValid ) { return; }
-    Auth.signup($scope.user)
-      .then(function (token) {
-        $window.localStorage.setItem('yobanfaUsername', $scope.user.username);
-        $state.go('friends');
-      })
-      .catch(function (error) {
-        alert("error");
-      });
+    // if( !isValid ) { return; }
+    // Auth.signup($scope.user)
+    //   .then(function (token) {
+    //     $window.localStorage.setItem('yobanfaUsername', $scope.user.username);
+    //     $state.go('friends');
+    //   })
+    //   .catch(function (error) {
+    //     alert("error");
+    //   });
+    console.log('signin!!!');
   };
+  $scope.something = function(){
+    console.log('something!!!');
+  };
+  $scope.$watch(localStorage['FBuserName'], function(newVal, oldVal){
+    //Here your view content is fully loaded !!
+    var id = localStorage.getItem('FBuserID');
+    var name = localStorage.getItem('FBuserName');
+    console.log('frontpage friend userID: ' + name +': ' + newVal);
+  });
 
 /*  $scope.enter = function() {
     $state.go('friends');
